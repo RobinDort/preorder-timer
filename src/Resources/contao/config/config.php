21385e67cd782c2ce3326e4c2b5a5a4046f20ba0
RobinDort <robindort@gmx.de>
2024-08-02 11:24:32 +0200

    Updated indices so the new logic will be placed 2 steps before the last checkout step

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,8 +3,8 @@ use RobinDort\PreorderTimer\Backend\CheckoutStep\IsotopePreorderTime;
 
 // Add customer notes before the last step (review).
 $checkoutStepCount = count($GLOBALS['ISO_CHECKOUTSTEP']);
-$length = $checkoutStepCount - 1;
-$offset = $checkoutStepCount - 1;
+$length = $checkoutStepCount - 2;
+$offset = $checkoutStepCount - 2;
 $preorderTime = ['preorder_time' => [IsotopePreorderTime::class]];
 $newCheckoutSteps = array_slice($GLOBALS['ISO_CHECKOUTSTEP'], 0, $length, TRUE) + $preorderTime + array_slice($GLOBALS['ISO_CHECKOUTSTEP'], $offset, $length, TRUE);
 $GLOBALS['ISO_CHECKOUTSTEP'] = $newCheckoutSteps;
