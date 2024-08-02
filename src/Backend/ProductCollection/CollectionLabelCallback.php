25c66e817c06ddd70836ab0cab21ab164b7959b9
RobinDort <robindort@gmx.de>
2024-08-02 11:08:37 +0200

    fixed an issue where a ? sign was missing for the php code to function properly

--- src/Backend/ProductCollection/CollectionLabelCallback.php
+++ src/Backend/ProductCollection/CollectionLabelCallback.php
@@ -15,7 +15,7 @@ class CollectionLabelCallback extends Callback {
 
         $objOrder = Order::findByPk($row['id']);
         $labelMarkup = '<span style="display: block; text-align: center;"><img src="system/themes/flexible/icons/ICONNAME.svg" width="16" height="16"></span>';
-        if (!empty($objOrder->preorder_time)) {
+        if (!empty($objOrder->preorder_time && $objOrder->preorder_time !== NULL)) {
             $args[] = str_replace('ICONNAME', 'ok', $labelMarkup);
         }
         else {
