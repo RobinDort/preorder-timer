70f5f4aa5f063f51f92b3bd28ae09fdec5c08f42
RobinDort <robindort@gmx.de>
2024-08-02 11:47:30 +0200

    Updated the newcheckoutsteps

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,10 +3,11 @@ use RobinDort\PreorderTimer\Backend\CheckoutStep\IsotopePreorderTime;
 
 // Add customer notes before the last step (review).
 $checkoutStepCount = count($GLOBALS['ISO_CHECKOUTSTEP']);
-$length = $checkoutStepCount - 2;
-$offset = $checkoutStepCount - 2;
+$insertPosition = $checkoutStepCount - 2;
+$firstPart = array_slice($GLOBALS['ISO_CHECKOUTSTEP'], 0, $insertPosition, true);
+$secondPart = array_slice($GLOBALS['ISO_CHECKOUTSTEP'], $insertPosition, null, true);
 $preorderTime = ['preorder_time' => [IsotopePreorderTime::class]];
-$newCheckoutSteps = array_slice($GLOBALS['ISO_CHECKOUTSTEP'], 0, $length, TRUE) + $preorderTime + array_slice($GLOBALS['ISO_CHECKOUTSTEP'], $offset, $length, TRUE);
+$newCheckoutSteps = $firstPart + $preorderTime + $secondPart;
 $GLOBALS['ISO_CHECKOUTSTEP'] = $newCheckoutSteps;
 
 
