113b3da0439122984b3b987dbb79d621c5dc2c8c
RobinDort <robindort@gmx.de>
2024-08-05 14:07:37 +0200

    Added a missing use statement

--- src/Widget/Frontend/PreorderFormular.php
+++ src/Widget/Frontend/PreorderFormular.php
@@ -2,6 +2,7 @@
 namespace RobinDort\PreorderTimer\Widget\Frontend;
 use Contao\Widget;
 use Haste\DateTime\DateTime;
+use SimplePie\Parse\Date;
 
 class PreorderFormular extends Widget {
     protected $blnSubmitInput = true;
