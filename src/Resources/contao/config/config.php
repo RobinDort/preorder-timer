be1c631cff14b7fc172ba0abded1fb8a4a28c1b7
RobinDort <robindort@gmx.de>
2024-08-07 09:03:51 +0200

    Using the asset helper tags now

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,8 +2,8 @@
 use RobinDort\PreorderTimer\Backend\CheckoutStep\IsotopePreorderTime;
 use RobinDort\PreorderTimer\Widget\Frontend\PreorderFormular;
 
-$GLOBALS['TL_CSS'][] = "bundles/preorder-timer/flatpickr.css";
-$GLOBALS['TL_JAVASCRIPT'][] = "bundles/preorder-timer/flatpickr.js";
+$GLOBALS['TL_CSS'][] = "bundles/preorder-timer/flatpickr.css|static";
+$GLOBALS['TL_JAVASCRIPT'][] = "bundles/preorder-timer/flatpickr.js|static";
 $GLOBALS['TL_FFL']['preorder_formular'] = PreorderFormular::class;  
 
 // Add customer notes before the last step (review).
