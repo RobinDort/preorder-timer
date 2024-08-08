f50dd7eb4fa8c3c48775d7d354bde844f150353e
RobinDort <robindort@gmx.de>
2024-08-08 12:19:04 +0200

    The PreorderLimiter class now has a function that returns a certain amount of preorders for a given datetime. The PreoderFormular class now has the validator function where the amount of orders are used to return an error if the max amount of orders for a given datetime has been fullfilled

--- src/Backend/Validation/PreorderLimiter.php
+++ src/Backend/Validation/PreorderLimiter.php
@@ -7,5 +7,15 @@ use Contao\Database;
 
 class PreorderLimiter {
 
+	private const PREORDER_STMT = "SELECT COUNT(*) FROM `tl_iso_product_collection` WHERE type='order' AND shipping_id != 28 AND preorder_time = ?";
+
+	public function countPreordersForDateTime($dateTime) {
+		$preordersResult = Database::getInstance()
+		->prepare(self::PREORDER_STMT)
+		->execute($dateTime)
+		->fetchAssoc();
+
+		return $preordersResult;
+	}
 }
 ?>
--- src/Widget/Frontend/PreorderFormular.php
+++ src/Widget/Frontend/PreorderFormular.php
@@ -7,16 +7,19 @@ use Contao\Widget;
 use Contao\Date;
 use Contao\Input;
 
+
 class PreorderFormular extends Widget {
     protected $blnSubmitInput = true;
     protected $blnForAttribute = true;
     protected $strTemplate = 'iso_checkout_preorder_time_formular';
     protected $strPrefix = 'widget widget-preorder-formular';
 
-    private $dayValidator;
+    private $preorderLimiter;
+
+    private const MAX_AMOUNT_SHIPPING_ORDERS = 2;
 
     public function __construct() {
-		$this->dayValidator = new DayValidator();
+		$this->preorderLimiter = new PreorderLimiter();
 	}
 
     /**
@@ -26,6 +29,39 @@ class PreorderFormular extends Widget {
     {
         // Not actually used
         return '';
+    }
+
+
+    /**
+     * {@inheritdoc}.
+     */
+    protected function validator($varInput) {
+        $dateValue = Input::post("date-input");
+        $timeValue = Input::post("time-input");
+
+        $dateTimeString = $dateValue . ' ' . $timeValue;
+        $expectedFormat = 'd.m.Y H:i';
+
+        $dateTime = \DateTime::createFromFormat($expectedFormat, $dateTimeString);
+        $errors = \DateTime::getLastErrors();
+
+        if ($dateTime === false || $errors['warning_count'] > 0 || $errors['error_count'] > 0) {
+            $errorMessage = 'Invalid date or time format. Please use the format: ' . $expectedFormat;
+            \System::log($errorMessage . ' - Date input: ' . $dateValue . ', Time input: ' . $timeValue, __METHOD__, TL_ERROR);
+            return $varInput;
+        }
+
+        //datetime is in valid format
+        $dateTimeTimestamp = $dateTime->getTimestamp();
+        $preorderCountForDateTime = $this->preorderLimiter->countPreordersForDateTime($dateTimeTimestamp);
+
+        if($preorderCountForDateTime > self::MAX_AMOUNT_SHIPPING_ORDERS) {
+            $errorMessage = "Wir bedauern, Ihnen mitteilen zu müssen, dass für den von Ihnen gewünschten Zeitraum bereits zu viele Vorbestellungen eingegangen sind. Wir bitten Sie daher einen anderen Zeitraum für Ihre Bestellung auszuwählen.";
+            $this->addError($errorMessage);
+            return $varInput;
+        }
+
+        return $dateTime->getTimestamp();
     }
 }
 
