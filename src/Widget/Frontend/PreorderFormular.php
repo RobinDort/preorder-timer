25830cc813053fb742ca959313d9f28705a7231c
RobinDort <robindort@gmx.de>
2024-08-08 12:40:52 +0200

    shortly removed the new function for debugging

--- src/Widget/Frontend/PreorderFormular.php
+++ src/Widget/Frontend/PreorderFormular.php
@@ -32,39 +32,7 @@ class PreorderFormular extends Widget {
     }
 
 
-    /**
-     * {@inheritdoc}.
-     */
-    protected function validator($varInput) {
-        $dateValue = Input::post("date-input");
-        $timeValue = Input::post("time-input");
 
-        $dateTimeString = $dateValue . ' ' . $timeValue;
-        $expectedFormat = 'd.m.Y H:i';
-
-        $dateTime = \DateTime::createFromFormat($expectedFormat, $dateTimeString);
-        $errors = \DateTime::getLastErrors();
-
-        if ($errors !== false) { // ensures the function did not return false and thus an error of errors
-            if ($dateTime === false || $errors['warning_count'] > 0 || $errors['error_count'] > 0) {
-                $errorMessage = 'Invalid date or time format. Please use the format: ' . $expectedFormat;
-                \System::log($errorMessage . ' - Date input: ' . $dateValue . ', Time input: ' . $timeValue, __METHOD__, TL_ERROR);
-                return $varInput;
-            }
-        }   
-
-        //datetime is in valid format
-        $dateTimeTimestamp = $dateTime->getTimestamp();
-        $preorderCountForDateTime = $this->preorderLimiter->countPreordersForDateTime($dateTimeTimestamp);
-
-        if($preorderCountForDateTime > self::MAX_AMOUNT_SHIPPING_ORDERS) {
-            $errorMessage = "Wir bedauern, Ihnen mitteilen zu müssen, dass für den von Ihnen gewünschten Zeitraum bereits zu viele Vorbestellungen eingegangen sind. Wir bitten Sie daher einen anderen Zeitraum für Ihre Bestellung auszuwählen.";
-            $this->addError($errorMessage);
-            return $varInput;
-        }
-
-        return $dateTime->getTimestamp();
-    }
 }
 
 ?>
