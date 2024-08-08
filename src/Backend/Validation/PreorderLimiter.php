46671e8d6a3f4cdd2a173125a5ed0fca67974c77
RobinDort <robindort@gmx.de>
2024-08-08 15:29:00 +0200

    The fetch will now be executed after the statement has finished

--- src/Backend/Validation/PreorderLimiter.php
+++ src/Backend/Validation/PreorderLimiter.php
@@ -10,25 +10,21 @@ class PreorderLimiter {
 	public function countPreordersForDateTime($dateTime) {
 		$preorderStmt = "SELECT COUNT(*) FROM `tl_iso_product_collection` AS total_count WHERE type='order' AND shipping_id != 28 AND preorder_time = ?";
 
-		\System::log("dateTime" . $dateTime, __METHOD__, TL_ERROR);
 		
 		$preordersResult = Database::getInstance()
 		->prepare($preorderStmt)
-		->execute($dateTime)
-		->fetchField();
+		->execute($dateTime);
+		
+		
+		$preorderCount = $preordersResult->fetchField();
 
-		\System::log("preordersResult" . $preordersResult, __METHOD__, TL_ERROR);
+		\System::log("preorderCount" . $preorderCount, __METHOD__, TL_ERROR);
 
 
 		throw new \Exception("preordersResult" . $preordersResult);
-
-		// Check if the result is not empty and retrieve the count
-		if (!empty($preordersResult) && isset($preordersResult['total_count'])) {
-			return (int) $preordersResult['total_count'];
-		}
 	
 		// Return 0 if no result is found
-		return 0;
+		return $preorderCount;
 	}
 }
 ?>
