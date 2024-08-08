9b50d9ba17751d8b3dc9177d977b4c90bb151f3f
RobinDort <robindort@gmx.de>
2024-08-08 08:37:50 +0200

    added an array merge instead of the + operator

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -15,10 +15,12 @@ $GLOBALS['TL_FFL']['preorder_formular'] = PreorderFormular::class;
 // Add customer notes before the last step (review).
 $checkoutStepCount = count($GLOBALS['ISO_CHECKOUTSTEP']);
 $insertPosition = $checkoutStepCount - 2;
+
 $firstPart = array_slice($GLOBALS['ISO_CHECKOUTSTEP'], 0, $insertPosition, true);
 $secondPart = array_slice($GLOBALS['ISO_CHECKOUTSTEP'], $insertPosition, null, true);
 $preorderTime = ['preorder_time' => [IsotopePreorderTime::class]];
-$newCheckoutSteps = $firstPart + $preorderTime + $secondPart;
+
+$newCheckoutSteps = array_merge($firstPart, $preorderTime, $secondPart);
 $GLOBALS['ISO_CHECKOUTSTEP'] = $newCheckoutSteps;
 
 
