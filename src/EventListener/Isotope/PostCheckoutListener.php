9b42390263631f9fb9eb36bef953fb393ccc6f54
RobinDort <robindort@gmx.de>
2024-08-12 14:24:16 +0200

    Removed the debugging statements again

--- src/EventListener/Isotope/PostCheckoutListener.php
+++ src/EventListener/Isotope/PostCheckoutListener.php
@@ -17,10 +17,6 @@ class PostCheckoutListener
             // Find the "Vorbestellung" order status
             $preorderStatus = OrderStatus::findOneBy('name', self::PRE_ORDER_OBJ_STATUS_NAME);
             
-            \System::log($preorderStatus->id,__METHOD__,TL_ERROR);
-            \System::log(print_r($objOrder->order_status),__METHOD__,TL_ERROR);
-            throw new \Exception("ObjOrder " . $objOrder);
-            
             if ($preorderStatus !== null) {
                 // Update the order status
                 $objOrder->order_status = $preorderStatus->id;
