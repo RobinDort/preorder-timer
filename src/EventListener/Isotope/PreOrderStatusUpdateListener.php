596a66ec9c47e97e0a7572ed49382b72fc9a2e9a
RobinDort <robindort@gmx.de>
2024-08-13 12:17:23 +0200

    Debugging

--- src/EventListener/Isotope/PreOrderStatusUpdateListener.php
+++ src/EventListener/Isotope/PreOrderStatusUpdateListener.php
@@ -28,6 +28,11 @@ class PreOrderStatusUpdateListener
                 // Update the order status
                 $order->order_status = $newStatus;
                 $order->save();
+                \System::log($newStatus,__METHOD__,TL_ERROR);
+                \System::log($order->order_status,__METHOD__,TL_ERROR);
+                \System::log($updates,__METHOD__,TL_ERROR);
+
+                throw new \Exception("order status: " . $newStatus);
             } 
 
         }
