e13628cd2684e93d8373724f2b8435ef447d254c
RobinDort <robindort@gmx.de>
2024-08-13 12:25:50 +0200

    Moved the debugging messages

--- src/EventListener/Isotope/PreOrderStatusUpdateListener.php
+++ src/EventListener/Isotope/PreOrderStatusUpdateListener.php
@@ -21,18 +21,21 @@ class PreOrderStatusUpdateListener
         // Check if preorder_time is set
         if ($order->preorder_time) {
             // Find the "Vorbestellung" order status
-            $preorderStatus = OrderStatus::findBy('name', self::PRE_ORDER_OBJ_STATUS_NAME);
+            $preorderStatus = OrderStatus::findOneBy('name', self::PRE_ORDER_OBJ_STATUS_NAME);
+           
+                \System::log($preorderStatus,__METHOD__,TL_ERROR);
+                \System::log($newStatus,__METHOD__,TL_ERROR);
+                \System::log($order->order_status,__METHOD__,TL_ERROR);
+                \System::log($updates,__METHOD__,TL_ERROR);
+
+                throw new \Exception("order status: " . $newStatus);
             
             if ($preorderStatus !== null && $order->preorder_time) {
                 $newStatus = $preorderStatus;
                 // Update the order status
                 $order->order_status = $newStatus;
                 $order->save();
-                \System::log($newStatus,__METHOD__,TL_ERROR);
-                \System::log($order->order_status,__METHOD__,TL_ERROR);
-                \System::log($updates,__METHOD__,TL_ERROR);
-
-                throw new \Exception("order status: " . $newStatus);
+                
             } 
 
         }
