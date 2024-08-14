280a962930b1844d5756a660296fd00c9a0c3e0f
RobinDort <robindort@gmx.de>
2024-08-14 08:47:30 +0200

    fixed a typo inside the template and added a debug statement

--- src/Notification/PreorderTimeTokenProvider.php
+++ src/Notification/PreorderTimeTokenProvider.php
@@ -8,6 +8,8 @@ class PreorderTimeTokenProvider {
     {
         // Assuming the preorder_time is stored as a timestamp in the order
         if ($order->preorder_time) {
+            \System::log("preorder_time hook " . $order->preorder_time,__METHOD__,TL_ERROR);
+            throw new \Exception("order" . $order);
             $dateTime = \DateTime::createFromFormat('U', $order->preorder_time);
             $tokens['preorder_time'] = $dateTime->format('d.m.Y H:i');
         }
