7e4fb1c07bdd05f256852a4b7ad6a9b71a024fb2
RobinDort <robindort@gmx.de>
2024-08-14 10:21:41 +0200

    Updated the token condition so the template will not print when the time is empty

--- src/Notification/PreorderTimeTokenProvider.php
+++ src/Notification/PreorderTimeTokenProvider.php
@@ -8,7 +8,7 @@ class PreorderTimeTokenProvider {
     public function __invoke(Order $order, array $tokens): array
     {
         // Assuming the preorder_time is stored as a timestamp in the order
-        if ($order->preorder_time) {
+        if ($order->preorder_time && !empty($order->preorder_time)) {
             // Create a DateTime object from the timestamp in UTC
             $dateTime = \DateTime::createFromFormat('U', $order->preorder_time, new \DateTimeZone('UTC'));
 
@@ -17,6 +17,8 @@ class PreorderTimeTokenProvider {
 
             // Format the adjusted date and time for the token
             $tokens['preorder_time'] = $dateTime->format('d.m.Y H:i');
+        } else {
+            $tokens['preorder_time'] = '';
         }
 
         return $tokens;
