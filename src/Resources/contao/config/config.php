5077f2026a4c9997ca1a4dc9e6f02e72e6d91619
RobinDort <robindort@gmx.de>
2024-08-19 10:00:46 +0200

    Readded the hooks and removed the custom order status

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -26,15 +26,6 @@ $preorderStatus = new OrderStatus();
 // First check if entry already exists
 $existingStatus = OrderStatus::findBy('name', 'Vorbestellung');
 
-// Create a new order status with sorting 312. (Sorting can be changed according to the users database sorting order) 
-if ($existingStatus === null) {
-    $preorderStatus->name = "Vorbestellung";
-    $preorderStatus->color = "ff00ff";
-    $preorderStatus->tstamp = time();
-    $preorderStatus->sorting = 312;
-    $preorderStatus->save();
-}
-
 // Add customer notes before the last step (review).
 $checkoutStepCount = count($GLOBALS['ISO_CHECKOUTSTEP']);
 $insertPosition = $checkoutStepCount - 2;
@@ -45,4 +36,10 @@ $preorderTime = ['preorder_time' => [IsotopePreorderTime::class]];
 
 $newCheckoutSteps = array_merge($firstPart, $preorderTime, $secondPart);
 $GLOBALS['ISO_CHECKOUTSTEP'] = $newCheckoutSteps;
+
+// Update the order status and call the hook to preorder when a preorder_time has been set
+$GLOBALS['ISO_HOOKS']['preOrderStatusUpdate'][] = [PreOrderStatusUpdateListener::class, '__invoke'];
+
+// Init a new notification center token that provides the preorder_time so it can be attached to the billings.
+$GLOBALS['ISO_HOOKS']['getOrderNotificationTokens'][] = [PreorderTimeTokenProvider::class,'__invoke'];
 ?>
