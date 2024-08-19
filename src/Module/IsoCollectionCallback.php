3265b5c630bf74999124776326acbebef4e59528
RobinDort <robindort@gmx.de>
2024-08-19 15:10:30 +0200

    Adjusted the presentation of the preorder time inside the backend. The time will now be displayed with 2 hours + because of the different UTC

--- src/Module/IsoCollectionCallback.php
+++ src/Module/IsoCollectionCallback.php
@@ -7,8 +7,19 @@ class IsoCollectionCallback extends Backend {
     public static function formatPreorderTime($value, \Contao\DataContainer $dc)
     {
         if ($value) {
+            // Create a DateTime object from the timestamp
             $dateTime = \DateTime::createFromFormat('U', $value);
-            return $dateTime ? $dateTime->format('d.m.Y H:i') : 'Ungültiges Datum';
+            
+            // Check if the DateTime object was created successfully
+            if ($dateTime) {
+                // Add 2 hours to the time to account for Germany's timezone (UTC+2)
+                $dateTime->modify('+2 hours');
+                
+                // Return the formatted date and time
+                return $dateTime->format('d.m.Y H:i');
+            } else {
+                return 'Ungültiges Datum';
+            }
         }
         return 'Keine Vorbestellung';
     }
