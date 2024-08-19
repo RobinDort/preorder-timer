313e1ab3f4d1401fd0bc62bf7208d1ee70b20962
RobinDort <robindort@gmx.de>
2024-08-19 12:00:41 +0200

    removed the help tip from the dca container for the preorder_time

--- src/Resources/contao/dca/tl_iso_product_collection.php
+++ src/Resources/contao/dca/tl_iso_product_collection.php
@@ -18,11 +18,16 @@ $GLOBALS['TL_DCA']['tl_iso_product_collection']['fields']['preorder_time'] = [
     'label'       => &$GLOBALS['TL_LANG']['tl_iso_product_collection']['preorder_time'],
     'exclude'     => TRUE,
     'inputType'   => 'text', // Using text as it’s non-interactive
-    'eval'        => ['rgxp'=>'digit', 'readonly'=>true, 'tl_class'=>'clr'],
-    'load_callback' => [
+    'eval'        => [
+        'rgxp'=>'digit',
+        'readonly'=>true,
+        'tl_class'=>'clr',
+        'help'=>false,
+    ],
+        'load_callback' => [
         [IsoCollectionCallback::class, 'formatPreorderTime']
     ],
-    'sql'         => "int(10) unsigned NULL",
+    'sql'=>"int(10) unsigned NULL",
 ];
 
 ?>  
