1ca496dc6ae7691646ebf9349f8eb0afdb6af03e
RobinDort <robindort@gmx.de>
2024-08-19 11:54:51 +0200

    Fixed a wrong class call

--- src/Resources/contao/dca/tl_iso_product_collection.php
+++ src/Resources/contao/dca/tl_iso_product_collection.php
@@ -20,7 +20,7 @@ $GLOBALS['TL_DCA']['tl_iso_product_collection']['fields']['preorder_time'] = [
     'inputType'   => 'text', // Using text as it’s non-interactive
     'eval'        => ['rgxp'=>'digit', 'readonly'=>true, 'tl_class'=>'clr'],
     'load_callback' => [
-        ['IsoCollectionCallback', 'formatPreorderTime']
+        [IsoCollectionCallback::class, 'formatPreorderTime']
     ],
     'sql'         => "int(10) unsigned NULL",
 ];
