ae29abbf50b7cd3f11642956127363742096840f
RobinDort <robindort@gmx.de>
2024-08-22 14:18:58 +0200

    Instead of the normal response now a json response will be used

--- src/Controller/HolidayRequestController.php
+++ src/Controller/HolidayRequestController.php
@@ -3,13 +3,13 @@ namespace RobinDort\PreorderTimer\Controller;
 use RobinDort\PreorderTimer\Widget\Frontend\Helper\HolidayCalculation;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/holidayRequest', name: HolidayRequestController::class, defaults: ['_scope' => 'frontend', '_token_check' => true])]
 class HolidayRequestController
 {
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request): JsonResponse
     {
         $extractedDate = $request->request->get('date');
         $holidayHelper = new HolidayCalculation();
@@ -19,7 +19,7 @@ class HolidayRequestController
             $response["isHoliday"] = true;
         }
 
-        return new Response($response);
+        return new JsonResponse($response);
     }
 }
 
