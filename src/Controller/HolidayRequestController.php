d7ba8c055ac13cfe4785c4a8b86ca35826246bba
RobinDort <robindort@gmx.de>
2024-08-22 13:27:20 +0200

    added a scope and token protection to the controller class

--- src/Controller/HolidayRequestController.php
+++ src/Controller/HolidayRequestController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/holidayRequest', name: HolidayRequestController::class)]
+#[Route('/holidayRequest', name: HolidayRequestController::class, defaults: ['_scope' => 'frontend', '_token_check' => true])]
 class HolidayRequestController
 {
     public function __invoke(Request $request): Response
