790a5e27a988337fd00145965260de01937fe798
RobinDort <robindort@gmx.de>
2024-08-28 14:42:07 +0200

    Added logging to the backend callback

--- src/Backend/ProductCollection/CollectionLabelCallback.php
+++ src/Backend/ProductCollection/CollectionLabelCallback.php
@@ -16,6 +16,8 @@ class CollectionLabelCallback extends Callback {
         $objOrder = Order::findByPk($row['id']);
         $labelMarkup = '<span style="display: block; text-align: center;"><img src="system/themes/flexible/icons/ICONNAME.svg" width="16" height="16"></span>';
         if (!empty($objOrder->preorder_time && $objOrder->preorder_time !== NULL)) {
+            \System::log("preorder time: " . $objOrder->preorder_time,__METHOD__,TL_ERROR);
+            throw new \Exception("objOrderTime: " . $objOrder->preorder_time);
             $args[] = str_replace('ICONNAME', 'ok', $labelMarkup);
         }
         else {
