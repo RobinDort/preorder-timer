184b831437777405b801305ab4ff98525cf57059
RobinDort <robin@alexanderdort.com>
2024-10-28 13:11:55 +0100

    Added new validation for the time picker to get the next available preorder time on init

---
+++ src/Controller/CountPreorderRequestController.php
@@ -0,0 +1,40 @@
+<?php
+namespace RobinDort\PreorderTimer\Controller;
+use RobinDort\PreorderTimer\Widget\Backend\Validation\PreorderLimiter;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/countPreorderRequest', name: CountPreorderRequestController::class, defaults: ['_scope' => 'frontend', '_token_check' => true])]
+class CountPreorderRequestController
+{
+    private const MAX_AMOUNT_SHIPPING_ORDERS = 1;
+    private const MAX_AMOUNT_PICK_UP_ORDERS = 2;
+
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $extractedTime = trim($request->request->get('time'), '"');
+        $extractedIsShippingOrder = trim($request->request->get('isShippingOrder'), '"');
+
+        $preorderLimiter = new PreorderLimiter();
+        $preorderCount = $preorderLimiter->countPreordersForDateTime($extractedTime, $extractedIsShippingOrder);
+
+        $response = ["availablePreorderTime"=>$extractedTime];
+
+        if ($extractedIsShippingOrder === true && $preorderCount >= self::MAX_AMOUNT_SHIPPING_ORDERS) {
+            $newAvailablePreorderTime = $preorderLimiter->findNextAvailableBookingTime($extractedTime, $extractedIsShippingOrder);
+            $response["availablePreorderTime"] = $newAvailablePreorderTime;
+
+
+        } else if ($extractedIsShippingOrder === false && $preorderCount >= self::MAX_AMOUNT_PICK_UP_ORDERS) {
+            $newAvailablePreorderTime = $preorderLimiter->findNextAvailableBookingTime($extractedTime, $extractedIsShippingOrder);
+            $response["availablePreorderTime"] = $newAvailablePreorderTime;
+        }
+
+        return new JsonResponse($response);
+    }
+}
+
+?>
