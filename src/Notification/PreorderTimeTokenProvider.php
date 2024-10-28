dd28bad8b14002ba820c1b29331e8d8464e7fc06
RobinDort <robin@alexanderdort.com>
2024-10-28 08:45:01 +0100

    removed the 2 extra hours from the preorder_token

--- src/Notification/PreorderTimeTokenProvider.php
+++ src/Notification/PreorderTimeTokenProvider.php
@@ -9,10 +9,10 @@ class PreorderTimeTokenProvider {
         // Assuming the preorder_time is stored as a timestamp in the order
         if ($order->preorder_time && !empty($order->preorder_time)) {
             // Create a DateTime object from the timestamp in UTC
-            $dateTime = \DateTime::createFromFormat('U', $order->preorder_time, new \DateTimeZone('UTC'));
+            $dateTime = \DateTime::createFromFormat('U', $order->preorder_time, new \DateTimeZone('Europe/Berlin'));
 
             // Adjust to German timezone (Europe/Berlin) by adding 2 hours
-            $dateTime->modify('+2 hours');
+            // $dateTime->modify('+2 hours');
 
             // Format the adjusted date and time for the token
             $tokens['preorder_time'] = $dateTime->format('d.m.Y H:i');
