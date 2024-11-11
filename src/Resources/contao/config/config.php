7ab40b2ef06716feec071461fa8668375e386250
RobinDort <robin@alexanderdort.com>
2024-11-11 15:18:13 +0100

    Added icon to the backend module

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -64,6 +64,7 @@ $GLOBALS['ISO_HOOKS']['getOrderNotificationTokens'][] = [PreorderTimeTokenProvid
 
 $GLOBALS['BE_MOD']['Vorbestellungen Konfiguration']['Shopzeiten'] = [
     'tables'    => ['tl_preorder_settings'],
+    'icon'      => '../../system/themes/flexible/icons/wrench.svg',
     'callback'  => PreorderTimerSettings::class,
 ];
 ?>
