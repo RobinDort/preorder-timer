17bd3f57b7e598a0e63c17d60b36e6fed987d761
RobinDort <robin@alexanderdort.com>
2024-11-11 14:59:42 +0100

    Added a request token to the form in order to be able to submit

--- src/Backend/PreorderSettings/PreorderTimerSettings.php
+++ src/Backend/PreorderSettings/PreorderTimerSettings.php
@@ -25,6 +25,12 @@ class PreorderTimerSettings extends BackendModule {
     public function compile() {}
 
     public function generate() {
+         // Validate the form submission and the CSRF token
+         if (Input::post('FORM_SUBMIT') === 'tl_preorder_settings_form' && !Input::post('REQUEST_TOKEN') || !Security::isValidToken(Input::post('REQUEST_TOKEN'))) {
+            // CSRF Token failed or missing
+            throw new Exception('Invalid CSRF Token');
+        }
+
         if (Input::post('FORM_SUBMIT') === 'tl_preorder_settings_form') {
             $preorderDate = Input::post('preorder_date_shop_closed');
 
