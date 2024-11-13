a524d5daa13146e7967a0982ef7f48f876ca03f3
RobinDort <robin@alexanderdort.com>
2024-11-13 09:43:10 +0100

    Fixed a typo inside the controller request

--- src/Controller/SpecialClosedDaysRequestController.php
+++ src/Controller/SpecialClosedDaysRequestController.php
@@ -8,8 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/removeClosedDayEntry', name: SpecialClosedDaysRequestController::class, defaults: ['_token_check' => true, '_scope' => 'backend'])]
 class SpecialClosedDaysRequestController {
     public function __invoke(Request $request): JsonResponse {
-        $entryDate = $request->request-get('entryDate');
-        $entryStatus = $request->request-get('entryStatus');
+        $entryDate = $request->request->get('entryDate');
+        $entryStatus = $request->request->get('entryStatus');
+
+        // Check if the parameters are set
+        if (!$entryDate || !$entryStatus) {
+            return new JsonResponse(['status' => 'error', 'message' => 'Invalid data provided'], 400);
+        }
     }
 }
 ?>
