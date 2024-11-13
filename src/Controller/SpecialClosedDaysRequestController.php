09a401953d4686663364a338ed274bc980263059
RobinDort <robin@alexanderdort.com>
2024-11-13 09:37:09 +0100

    Removed the debug exception

--- src/Controller/SpecialClosedDaysRequestController.php
+++ src/Controller/SpecialClosedDaysRequestController.php
@@ -10,9 +10,6 @@ class SpecialClosedDaysRequestController {
     public function __invoke(Request $request): JsonResponse {
         $entryDate = $request->request-get('entryDate');
         $entryStatus = $request->request-get('entryStatus');
-
-        \System::log("request data: " . $entryDate . "," . $entryStatus,__METHOD__,"TL_ERROR");
-        throw new \Exception("DEBUG");
     }
 }
 ?>
