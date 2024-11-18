e54f58d65197b049e35d9c2f8bb2e8363389e25a
RobinDort <robin@alexanderdort.com>
2024-11-18 14:56:06 +0100

    Removed extra exception for debug

--- src/Backend/CheckoutStep/IsotopePreorderTime.php
+++ src/Backend/CheckoutStep/IsotopePreorderTime.php
@@ -85,7 +85,7 @@ class IsotopePreorderTime extends CheckoutStep implements IsotopeCheckoutStep {
             } else {
                 //try {
                     $this->blnError = true;
-                    throw new \Exception($objWidget->getErrorsAsString());
+                  //  throw new \Exception($objWidget->getErrorsAsString());
               //  } catch (\Exception $e) {
                     \System::log("Exception: " .$e->getMessage(), __METHOD__, "TL_ERROR");
                     $objWidget->addError($e->getMessage());
