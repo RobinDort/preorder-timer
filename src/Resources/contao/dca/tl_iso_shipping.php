6bb7e64f253b875e1dcee3b1bb55195a0bdb7cad
RobinDort <robin@alexanderdort.com>
2025-05-19 13:02:49 +0200

    Adjusted dcas

--- src/Resources/contao/dca/tl_iso_shipping.php
+++ src/Resources/contao/dca/tl_iso_shipping.php
@@ -1,7 +1,7 @@
 <?php
 
 // Add new field to the default palette, after 'postalCodes'
-$GLOBALS['TL_DCA']['tl_iso_shipping']['palettes']['flat'] = str_replace(
+$GLOBALS['TL_DCA']['tl_iso_shipping']['palettes']['flat_city'] = str_replace(
     'postalCodes,',
     'postalCodes,postalCity,',
     $GLOBALS['TL_DCA']['tl_iso_shipping']['palettes']['flat']
@@ -10,7 +10,7 @@ $GLOBALS['TL_DCA']['tl_iso_shipping']['palettes']['flat'] = str_replace(
 
 // Define new postalCity the field
 $GLOBALS['TL_DCA']['tl_iso_shipping']['fields']['postalCity'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_iso_shipping']['postalCity'],
+    'label'     => &$GLOBALS['TL_LANG']['tl_iso_shipping']['postalCity'][0],
     'exclude'   => true,
     'inputType' => 'textarea',
     'eval'      => ['tl_class' => 'clr', 'style' => 'height:100px', 'decodeEntities' => true],
