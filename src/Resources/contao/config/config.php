65359fdd97df35faab6cf79266856ad84b39579d
RobinDort <robin@alexanderdort.com>
2025-06-24 11:22:44 +0200

    changed the position of the preorder time formular for testing purposes

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -45,7 +45,7 @@ if ($existingStatus === null) {
 
 // Add customer notes before the last step (review).
 $checkoutStepCount = count($GLOBALS['ISO_CHECKOUTSTEP']);
-$insertPosition = $checkoutStepCount - 2;
+$insertPosition = $checkoutStepCount - 5;
 
 $firstPart = array_slice($GLOBALS['ISO_CHECKOUTSTEP'], 0, $insertPosition, true);
 $secondPart = array_slice($GLOBALS['ISO_CHECKOUTSTEP'], $insertPosition, null, true);
