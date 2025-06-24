b1b023ad95e2b64e6b6b2e7e5a96e86aff057dbe
RobinDort <robin@alexanderdort.com>
2025-06-24 11:01:26 +0200

    printing the whole request obj

--- src/Controller/HolidayRequestController.php
+++ src/Controller/HolidayRequestController.php
@@ -11,8 +11,8 @@ class HolidayRequestController
 {
     public function __invoke(Request $request): JsonResponse
     {
-        \System::log("request:" . print_r($request->request), __METHOD__, "TL_ERROR");
-        throw new \Exception("request:" . print_r($request->request));
+        \System::log("request:" . print_r($request->request->all(), true), __METHOD__, "TL_ERROR");
+        throw new \Exception("request:" . print_r($request->request->all(), true));
 
         //$extractedDate = trim($request->request->get('date'), '"');
         $extractedDate = $request->request->get('date');
