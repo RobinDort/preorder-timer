91d7aab6fd1f5488018a30717a902b0377b9165c
RobinDort <75115265+RobinDort@users.noreply.github.com>
2025-11-17 13:54:31 +0100

    Updated the condition inside CollectionLabelCallback so the empty condition checks for the preOrderTime instead of a boolean condition

--- src/Backend/ProductCollection/CollectionLabelCallback.php
+++ src/Backend/ProductCollection/CollectionLabelCallback.php
@@ -15,7 +15,7 @@ class CollectionLabelCallback extends Callback {
 
         $objOrder = Order::findByPk($row['id']);
         $labelMarkup = '<span style="display: block; text-align: center;"><img src="system/themes/flexible/icons/ICONNAME.svg" width="16" height="16"></span>';
-        if (!empty($objOrder->preorder_time && $objOrder->preorder_time !== NULL)) {
+        if (!empty($objOrder->preorder_time)) {
             // Create a DateTime object from the Unix timestamp
             $date = new \DateTime('@' . $objOrder->preorder_time);
 
